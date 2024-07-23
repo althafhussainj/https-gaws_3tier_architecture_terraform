6aae4c106bcf2fdc1533017ff600ae9eeaa6b4b8
althafhussainj <140585466+althafhussainj@users.noreply.github.com>
2024-07-23 15:46:52 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,7 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.9.0"
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  
+  tags = {
+    Name = "CustomVPC"
+  }
 }
