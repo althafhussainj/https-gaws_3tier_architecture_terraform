7a0c172b7ac9fea0e399b9dae7ec7860a2af7e28
shabana syed <althafhussainj@gmail.com>
2024-07-23 11:57:11 +0530

    changed provider.tf

--- provider.tf
+++ provider.tf
@@ -1,18 +1,5 @@
-# Author    : Ranjit Kumar Swain
-# Web       : www.ranjitswain.com
-# YouTube   : https://www.youtube.com/c/ranjitswain
-# GitHub    : https://github.com/ranjit4github
-########################################################
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.20.1"
-    }
-  }
-}
-
 provider "aws" {
+  source = "hashicorp/aws"
+  version = "1.9.2"
   region = "ap-south-1"
 }
